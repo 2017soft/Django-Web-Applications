bc4c3d14f8c11ecd38d839118b711b25c7f386c3
Eric Luo <30668005+2017soft@users.noreply.github.com>
2018-03-27 17:26:06 -0400

    Add files via upload

--- first_django_project/mysite/hangman/tests.py
+++ first_django_project/mysite/hangman/tests.py
@@ -6,11 +6,22 @@ class SecretModelTests(TestCase):
     def test_default_parameters(self):
         check = True
         sample_word = Secret(secret_word = "apple")
+        if sample_word.secret_word != "apple":
+            check = False
         if sample_word.get_guesses_times() != 6:
             check = False
         if sample_word.get_warnings_times() != 3:
             check = False
         if sample_word.get_hints_times() != 2:
             check = False
-        
+        if len(sample_word.letters_guessed) != 0:
+            check = False
+        if len(sample_word.get_all_hints()) != 0:
+            check = False
+        if len(sample_word.available_letters) != 26:
+            check = False
+        if sample_word.check_guess() != 0:
+            check = False
+        if sample_word.check_game_success() != 0:
+            check = False
         self.assertIs(check, True)
